bce1e074a420130652c305b871679e457a264a94
Pavel <dementorpasha@gmail.com>
2025-09-21 12:41:12 +0300

    init commit

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+new Psa\Core\Web\App(
+    di: new Psa\Core\Common\Container(require_once __DIR__ . '/../src/Config/web.php'),
+    router: new Psa\Core\Web\Router(require_once __DIR__ . '/../src/Config/routes.php')
+)->run();
---
+++ src/Actions/HomePageAction.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Actions;
+
+class HomePageAction
+{
+    public function run()
+    {
+        return [
+            'title' => 'Home Page',
+        ];
+    }
+}
